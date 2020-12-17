c4afc2fd1ef4d0a20ebc4487128e9be51d4cfe21
Eelco <eelco.bosklopper@capgemini.com>
2020-12-17 12:05:23 +0100

    Tab cleanup

--- src/components/Tab.js
+++ src/components/Tab.js
@@ -1,45 +1,26 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 import * as microsoftTeams from "@microsoft/teams-js";
 
-/**
- * The 'GroupTab' component renders the main tab content
- * of your app.
- */
-class Tab extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      context: {}
-    }
-  }
+function Tab() {
+  const [context, setContext] = useState({});
 
-  //React lifecycle method that gets called once a component has finished mounting
-  //Learn more: https://reactjs.org/docs/react-component.html#componentdidmount
-  componentDidMount(){
-    // Get the user context from Teams and set it in the state
+  useEffect(() => {
     microsoftTeams.getContext((context, error) => {
-      this.setState({
-        context: context
-      });
+      setContext(context);
     });
-    // Next steps: Error handling using the error object
-  }
+  }, []);
 
-  render() {
+  let userName = Object.keys(context).length > 0 ? context["upn"] : "";
 
-      let userName = Object.keys(this.state.context).length > 0 ? this.state.context['upn'] : "";
-
-      return (
-      <div className="hello">
-        <h3>Hello World!!!!</h3>
-        <h1>Congratulations {userName}!</h1> <h3>This is the tab you made :-)</h3>
-        ;alskdjf;laksjdf
-      </div>
-      );
-  }
+  return (
+    <div className="hello">
+      <h3>Hello World!!!!</h3>
+      <h1>Congratulations {userName}!!</h1>
+      <h3>This is the tab you made :-)</h3>
+      ;alskdjf;laksjdf
+    </div>
+  );
 }
+
 export default Tab;
